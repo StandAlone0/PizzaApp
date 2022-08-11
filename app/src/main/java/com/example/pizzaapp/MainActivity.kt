798e869d278f9ef0f1c51e780f2cd131f07226a0
Evgeniy <evgeniymoroz1999@mail.ru>
2022-08-11 16:50:56 +0300

    Bottom View made

--- app/src/main/java/com/example/pizzaapp/MainActivity.kt
+++ app/src/main/java/com/example/pizzaapp/MainActivity.kt
@@ -2,10 +2,14 @@
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pizzaapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.bottomNav.selectedItemId = R.id.item1
     }
 }
